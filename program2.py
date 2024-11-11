22d9d1fcb6b34b86c8d0f0f488dbc7342fba9ed0
naveensangishetty3904 <148842039+naveensagishetty3904@users.noreply.github.com>
2024-11-11 13:40:25 +0530

    Update program2.py

--- program2.py
+++ program2.py
@@ -1,5 +1,30 @@
-def decode_message( s: str, p: str) -> bool:
+def decode_message(s: str, p: str) -> bool:
+    memo = {}
+    
+    def match(i, j):
+        if (i, j) in memo:
+            return memo[(i, j)]
+        
+        if j == len(p):
+            return i == len(s)
+        
+        if i == len(s):
+            memo[(i, j)] = p[j] == '*' and match(i, j + 1)
+            return memo[(i, j)]
+        
+        if p[j] == '*':
+            memo[(i, j)] = match(i + 1, j) or match(i, j + 1)
+        elif p[j] == '?' or p[j] == s[i]:
+            memo[(i, j)] = match(i + 1, j + 1)
+        else:
+            memo[(i, j)] = False
+        
+        return memo[(i, j)]
+    
+    return match(0, 0)
 
-# write your code here
-  
-        return False
+
+print(decode_message("aa", "a"))       
+print(decode_message("aa", "*"))       
+print(decode_message("cb", "?a"))      
+print(decode_message("aa", "a*"))      
