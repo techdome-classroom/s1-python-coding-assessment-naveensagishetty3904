a37aa0394bb6359f97544fae5e143f37223e3844
naveensangishetty3904 <148842039+naveensagishetty3904@users.noreply.github.com>
2024-11-11 14:04:25 +0530

    Update program2.py

--- program2.py
+++ program2.py
@@ -1,30 +1,14 @@
-def decode_message(s: str, p: str) -> bool:
-    memo = {}
-    
-    def match(i, j):
-        if (i, j) in memo:
-            return memo[(i, j)]
-        
-        if j == len(p):
-            return i == len(s)
-        
-        if i == len(s):
-            memo[(i, j)] = p[j] == '*' and match(i, j + 1)
-            return memo[(i, j)]
-        
-        if p[j] == '*':
-            memo[(i, j)] = match(i + 1, j) or match(i, j + 1)
-        elif p[j] == '?' or p[j] == s[i]:
-            memo[(i, j)] = match(i + 1, j + 1)
+def decode_message(message, key):
+    i = 0
+    j = 0
+    while i < len(message) and j < len(key):
+        if key[j] == '*':
+            while i < len(message) and j < len(key) - 1 and key[j + 1] != '*':
+                i += 1
+            j += 1
+        elif key[j] == '?' or message[i] == key[j]:
+            i += 1
+            j += 1
         else:
-            memo[(i, j)] = False
-        
-        return memo[(i, j)]
-    
-    return match(0, 0)
-
-
-print(decode_message("aa", "a"))       
-print(decode_message("aa", "*"))       
-print(decode_message("cb", "?a"))      
-print(decode_message("aa", "a*"))      
+            return False
+    return i == len(message) and j == len(key)
